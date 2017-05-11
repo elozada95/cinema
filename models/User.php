782936bf10fb31d7dbbf0496e0bc13a14f1e6879
elozada95 <enriquelozada95@hotmail.com>
2017-05-11 18:53:30 -0500

    escrinins

--- models/User.php
+++ models/User.php
@@ -4,8 +4,6 @@ require_once("../interfaces/IUser.php");
 class User implements IUser {
     private $con;
     private $id;
-    private $student;
-    private $professor;
     private $date;
     private $start;
     private $finish;
@@ -17,6 +15,11 @@ class User implements IUser {
     private $name;
     private $password;
     private $surname;
+    private $synopsis;
+    private $release;
+    private $duration;
+    private $genre;
+    private $rating;
     public function __construct(Database $db){
         $this->con = new $db;
     }
@@ -46,62 +49,26 @@ class User implements IUser {
     {
         $this->tipo = $tipo;
     }
-    public function setProfessor($professor){
-        $this->professor = $professor;
-    }
-    public function setDate($date){
-        $this->date = $date;
+    public function setDate($sdate){
+        $this->sdate = $sdate;
     }
     public function setStart($start){
         $this->start = $start;
     }
-    public function setFinish($finish){
-        $this->finish = $finish;
+    public function setSynopsis($synopsis){
+        $this->synopsis = $synopsis;
     }
-    public function setTopic($topic){
-        $this->topic = $topic;
-    }
-    public function setStudent($student){
-        $this->student= $student;
+    public function setRelease($synopsi){
+        $this->synopsis = $synopsis;
     }
     //insertamos usuarios en una tabla con postgreSql
-    public function save() {
-        try{
-            $query = $this->con->prepare('INSERT INTO users (username, password) values (?,?)');
-            $query->bindParam(1, $this->username, PDO::PARAM_STR);
-            $query->bindParam(2, $this->password, PDO::PARAM_STR);
-            $query->execute();
-            $this->con->close();
-        }
-        catch(PDOException $e) {
-            echo  $e->getMessage();
-        }
-    }
-    public function saveSchedule() {
-        try{
-            //$new =('id from hours order by id desc LIMIT 1');
-            //$new++;
-            //$query =$this->con->prepare('INSERT INTO hours (id,day, professor,start,finish,type,period) values (10,"Friday",01326798,"19:00","20:00","officeHour",1');
-            $query = $this->con->prepare('INSERT INTO hours (day, professor,start,finish,type,period) values (?,?,?,?,?,1)');
-            //$query->bindParam(1,$new);
-            $query->bindParam(1, $this->dia, PDO::PARAM_STR);
-            $query->bindParam(2, $this->professor, PDO::PARAM_STR);
-            $query->bindParam(3, $this->start, PDO::PARAM_STR);
-            $query->bindParam(4, $this->finish, PDO::PARAM_STR);
-            $query->bindParam(5, $this->tipo, PDO::PARAM_STR);  
-            $query->execute();
-            $this->con->close(); 
-        }
-        catch(PDOException $e) {
-            echo  $e->getMessage();
-        }
-    }
+
     public function saveUser() {
         try{
             //$new =('id from hours order by id desc LIMIT 1');
             //$new++;
             //$query =$this->con->prepare('INSERT INTO hours (id,day, professor,start,finish,type,period) values (10,"Friday",01326798,"19:00","20:00","officeHour",1');
-            $query = $this->con->prepare('INSERT INTO usuar (name,surname,email, password,type) values (?,?,?,?,1)');
+            $query = $this->con->prepare('INSERT INTO usuar (name,surname,email, password,type) VALUES (?,?,?,?,1)');
             //$query->bindParam(1,$new);
             $query->bindParam(1, $this->name, PDO::PARAM_STR);
             $query->bindParam(2, $this->surname, PDO::PARAM_STR);
@@ -149,6 +116,25 @@ class User implements IUser {
             echo  $e->getMessage();
         }
     }
+    public function saveMovie() {
+        try{
+            //$new =('id from hours order by id desc LIMIT 1');
+            //$new++;
+            //$query =$this->con->prepare('INSERT INTO hours (id,day, professor,start,finish,type,period) values (10,"Friday",01326798,"19:00","20:00","officeHour",1');
+            $query = $this->con->prepare('INSERT INTO movie(name, synopsis, releasedate, duration, gen, rating) VALUES (?,?,?,?,?,?)');
+            //$query->bindParam(1,$new);
+            $query->bindParam(1, $this->name, PDO::PARAM_STR);
+            $query->bindParam(2, $this->surname, PDO::PARAM_STR);
+            $query->bindParam(3, $this->email, PDO::PARAM_STR);
+            $query->bindParam(4, $this->password, PDO::PARAM_STR);
+            $query->execute();
+            $this->con->close();
+            
+        }
+        catch(PDOException $e) {
+            echo  $e->getMessage();
+        }
+    }
     public function saveAppointment() {
         try{
             $query = $this->con->prepare('INSERT INTO asesorias (student, professor, topic, dateh, start, finish) values (?, ?, ?, ?, ?, ?)');
@@ -156,7 +142,7 @@ class User implements IUser {
             $query->bindParam(2, $this->professor, PDO::PARAM_STR);
             $query->bindParam(3, $this->topic, PDO::PARAM_STR);
             $query->bindParam(4, $this->date, PDO::PARAM_STR);
-            $query->bindParam(5, $this->start, PDO::PARAM_STR);  
+            $query->bindParam(5, $this->start, PDO::PARAM_STR);
             $query->bindParam(6, $this->finish, PDO::PARAM_STR);
             $query->execute();
             $this->con->close(); 
@@ -322,26 +308,6 @@ class User implements IUser {
             echo  $e->getMessage();
         }
     }
-    public function get(){
-        try{
-            if(is_int($this->id)){
-                $query = $this->con->prepare('SELECT id FROM student order by id desc');
-                //$query->bindParam(1, $this->id, PDO::PARAM_INT);
-                $query->execute();
-                $this->con->close();
-                return $query->fetch(PDO::FETCH_OBJ);
-            }
-            else{
-                $query = $this->con->prepare('SELECT * FROM student');
-                $query->execute();
-                $this->con->close();
-                return $query->fetchAll(PDO::FETCH_OBJ);
-            }
-        }
-        catch(PDOException $e){
-            echo  $e->getMessage();
-        }
-    }
     public function getMovie(){
         try{
             if(is_int($this->id)){
@@ -397,7 +363,7 @@ class User implements IUser {
 
     public function getSinAdmin(){
         try{
-                $query = $this->con->prepare('SELECT * FROM usuar where id > 1 order by id asc');
+                $query = $this->con->prepare('SELECT * FROM usuar where id > 0 order by id asc');
                 //$query->bindParam(1, $this->id, PDO::PARAM_INT);
                 $query->execute();
                 $this->con->close();
@@ -408,44 +374,6 @@ class User implements IUser {
         }
     }
 
-    public function getNameSt(){
-        try{
-                $query = $this->con->prepare('SELECT * FROM student order by id asc ');
-                //$query->bindParam(1, $this->id, PDO::PARAM_INT);
-                $query->execute();
-                $this->con->close();
-                return $query->fetchAll(PDO::FETCH_OBJ);
-        }
-        catch(PDOException $e){
-            echo  $e->getMessage();
-        }
-    }
-
-    public function getNamePr(){
-        try{
-                $query = $this->con->prepare('SELECT * FROM professor order by id asc ');
-                //$query->bindParam(1, $this->id, PDO::PARAM_INT);
-                $query->execute();
-                $this->con->close();
-                return $query->fetchAll(PDO::FETCH_OBJ);
-        }
-        catch(PDOException $e){
-            echo  $e->getMessage();
-        }
-    }
-
-    public function delete(){
-        try{
-            $query = $this->con->prepare('DELETE FROM users WHERE id = ?');
-            $query->bindParam(1, $this->id, PDO::PARAM_INT);
-            $query->execute();
-            $this->con->close();
-            return true;
-        }
-        catch(PDOException $e){
-            echo  $e->getMessage();
-        }
-    }
      public function deleteUsuario(){
         try{
             $query = $this->con->prepare('DELETE FROM usuar WHERE id = ?');
