c20c0dd1eca525a0955abfaf234569efb9a90dab
Legabit <legabit@gmail.com>
2017-05-11 16:21:15 -0500

    muchas cosas

--- app/checkMovies.php
+++ app/checkMovies.php
@@ -13,24 +13,61 @@
     $users = $user->getMovie();
     ?>
     <div class="container">
-        <div class="col-lg-12">
-            <h2 class="text-center text-primary">Select a Movie</h2>
-            <div class="col-lg-6 col-lg-offset-3">
-                <form action="showMovie.php" method="post">
-                    <select name="movie" class="form-control">
+        <div class="col-lg-12"><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Movies</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+    <?php
+        require_once "../models/User.php";
+        session_start();
+        $type = $_SESSION['type'];
+        $paysheet = $_SESSION['paysheet'];
+        if($type != 1)
+        {
+            header("Location: logout.php");
+        }
+        $db = new Database;
+        $user = new User($db);
+        $users = $user->getMovie();
+    ?>
+    <div class="container" style="width:100%" align="center">
+        <div class="col-lg-12" style="width:100%" align="center">
+            <h2 class="text-center text-primary">Movies</h2>
+            <div style="width:80%" align="center">
+                <table class="table table-striped table-bordered" align="center">
+                    <thead>
+                        <tr>
+                            <th style="width:20%">Name</th>
+                            <th>Synopsis</th>
+                            <th style="width:10%">Release Date</th>
+                            <th style="width:5%">Duration</th>
+                            <th style="width:8%">Genre</th>
+                            <th style="width:7%">Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         <?php foreach( $users as $user ) {
-                            echo '<option value="'.$user->id.'">'.$user->name.'</option>';
+                            echo '<tr>';
+                                echo '<td>' .$user->name. '</td>';
+                                echo '<td>' .$user->synopsis. '</td>';
+                                echo '<td>' .$user->releasedate. '</td>';
+                                echo '<td>' .$user->duration. ' min</td>';
+                                echo '<td>' .$user->gen. '</td>';
+                                echo '<td>' .$user->rating. '</td>';
+                            echo '</tr>';
                         }
                         ?>
-                    </select>
-                    <br>
-                    <input class="btn btn-success col-lg-2 col-lg-offset-5" value="Continuar" type="submit">
-                </form>
+                    </tbody>
+                </table>     
             </div>
-            <div class="col-lg-12" style="margin-bottom: 100px">
+            <div class="col-lg-12" style="margin-bottom: 100px" align="center">
                 <br>
                 <br>
-                <a class="btn btn-info btn-block" href="usuario.php">Home</a>
+                <a class="btn btn-info btn-block" align="center"  style="width:10%" href="usuario.php">Home</a>
             </div>
         </div>
     </div>
---
+++ app/screeningsUser.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Movies</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+    <?php
+        require_once "../models/User.php";
+        session_start();
+        $type = $_SESSION['type'];
+        $paysheet = $_SESSION['paysheet'];
+        if($type != 1)
+        {
+            header("Location: logout.php");
+        }
+        $db = new Database;
+        $user = new User($db);
+        $users = $user->getMovie();
+    ?>
+    <div class="container" style="width:100%" align="center">
+        <div class="col-lg-12" style="width:100%" align="center">
+            <h2 class="text-center text-primary">Movies</h2>
+            <div style="width:80%" align="center">
+                <table class="table table-striped table-bordered" align="center">
+                    <thead>
+                        <tr>
+                            <th style="width:20%">Name</th>
+                            <th>Synopsis</th>
+                            <th style="width:10%">Release Date</th>
+                            <th style="width:5%">Duration</th>
+                            <th style="width:8%">Genre</th>
+                            <th style="width:7%">Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach( $users as $user ) {
+                            echo '<tr>';
+                                echo '<td>' .$user->name. '</td>';
+                                echo '<td>' .$user->synopsis. '</td>';
+                                echo '<td>' .$user->releasedate. '</td>';
+                                echo '<td>' .$user->duration. ' min</td>';
+                                echo '<td>' .$user->gen. '</td>';
+                                echo '<td>' .$user->rating. '</td>';
+                                echo '<td>';
+                                echo '&nbsp;';
+                                echo '<a class="btn btn-success" href="makeReservation.php? id='.$user->id.'">Make reservation</a>';
+                                echo '</td>';
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>     
+            </div>
+            <div class="col-lg-12" style="margin-bottom: 100px" align="center">
+                <br>
+                <br>
+                <a class="btn btn-info btn-block" align="center"  style="width:10%" href="admin.php">Home</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
