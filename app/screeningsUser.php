836a7f35467c0038e58f15110bc686bf48e60881
elozada95 <enriquelozada95@hotmail.com>
2017-05-12 03:22:49 -0500

    valida fecha

--- app/screeningsUser.php
+++ app/screeningsUser.php
@@ -7,20 +7,29 @@
 </head>
 <body>
     <?php
-        require_once "../models/User.php";
-        session_start();
-        $type = $_SESSION['type'];
-        $paysheet = $_SESSION['paysheet'];
-        if($type != 1)
-        {
-            header("Location: logout.php");
-        }
-        if(!$paysheet){
-            header("Location: logout.php");
-        }
-        $db = new Database;
-        $user = new User($db);
-        $users = $user->getScreening();
+    require_once "../models/User.php";
+    date_default_timezone_set('America/Mexico_City');
+    $today = date('Y-m-d',time());
+    $time = date('H:i:s',time());
+    $today = (string)$today;
+    $time = (string)$time;
+    $time = $today ." ". $time;
+    $time = strtotime($time);
+    $now = date('Y-m-d H:i:s', $time);
+    $now =time($now);
+    session_start();
+    $type = $_SESSION['type'];
+    $paysheet = $_SESSION['paysheet'];
+    if($type != 1)
+    {
+        header("Location: logout.php");
+    }
+    if(!$paysheet){
+        header("Location: logout.php");
+    }
+    $db = new Database;
+    $user = new User($db);
+    $users = $user->getScreening();
     ?>
     <div class="container" style="width:100%" align="center">
         <div class="col-lg-12" style="width:100%" align="center">
@@ -42,21 +51,29 @@
                     </thead>
                     <tbody>
                         <?php foreach( $users as $user ) {
+                            $dayf = (string)$user->sdate;
+                            $hourf = (string)$user->stime;
+                            $functime = $dayf ." ". $hourf;
+                            $functime = strtotime($functime);
+                            $functime = date('Y-m-d H:i:s', $functime);
+                            $functime = strtotime($functime);
                             echo '<tr>';
-                                echo '<td>' .$user->name. '</td>';
-                                echo '<td>' .$user->rating. '</td>';
-                                echo '<td>' .$user->idroom. '</td>';
-                                echo '<td>' .$user->type. '</td>';
-                                echo '<td>$' .$user->price. '</td>';
-                                echo '<td>' .$user->sdate. '</td>';
-                                echo '<td>' .$user->stime. '</td>';
+                            echo '<td>' .$user->name. '</td>';
+                            echo '<td>' .$user->rating. '</td>';
+                            echo '<td>' .$user->idroom. '</td>';
+                            echo '<td>' .$user->type. '</td>';
+                            echo '<td>$' .$user->price. '</td>';
+                            echo '<td>' .$user->sdate. '</td>';
+                            echo '<td>' .$user->stime. '</td>';
+                            if($functime>$now){
                                 echo '<td>';
                                 echo '&nbsp;';
                                 echo '<form action="makeReservation.php" method="post">';
-                                    echo '<input type="hidden" name="id" value="'.$user->id.'">';
-                                    echo '<input class="btn btn-success" value="Make Reservation" type="submit">';
+                                echo '<input type="hidden" name="id" value="'.$user->id.'">';
+                                echo '<input class="btn btn-success" value="Make Reservation" type="submit">';
                                 echo '</form>';
                                 echo '</td>';
+                            }
                             echo '</tr>';
                         }
                         ?>
